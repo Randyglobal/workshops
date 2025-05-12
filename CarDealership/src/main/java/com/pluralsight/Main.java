f5b28efd4b0b006f825454e9b842441f562c499b
Randyglobal <randyitoe2255@gmail.com>
2025-05-12 14:47:50 -0400

    Phase 4 in progress

--- CarDealership/src/main/java/com/pluralsight/Main.java
+++ CarDealership/src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,7 @@ import DealershipManager.UserInterface;
 
 public class Main {
     public static void main(String[] args) {
+//        Calling UI
         UserInterface ui = new UserInterface();
         ui.display();
 
